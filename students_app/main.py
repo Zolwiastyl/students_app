4e16b076608eccb48d864762bc02ffb67468eaf7
Jakub Kwiatek <jakub.zolw.kwiatek@gmail.com>
2022-04-21 16:51:45 +0200

    initialize students class

--- students_app/main.py
+++ students_app/main.py
@@ -2,11 +2,17 @@ from flask import Flask
 import os
 from dotenv import find_dotenv, load_dotenv
 
+from .models import *
+
 dotenv_path = os.path.join(os.path.dirname(__file__), "../.env")
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
---
+++ students_app/models/student.py
@@ -0,0 +1,10 @@
+from .base import db
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
+
+
+class Student(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
