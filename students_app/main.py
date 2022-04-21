015c5da6496e30ff89f3815ddf1ebb8a752c541c
Jakub Kwiatek <jakub.zolw.kwiatek@gmail.com>
2022-04-21 16:17:14 +0200

    setup db

--- students_app/main.py
+++ students_app/main.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import os
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello</p>"
+    print(os.environ.get("DATABASE_URI"))
+    return "s"
